0b4a52fc51f6a8c725426fc148434c073c2fa891
Elizabeth Guan <yijie.beth.guan@gmail.com>
2024-11-07 00:22:55 -0800

    Starting assignment 2

---
+++ 2_style/Auto_UV.py
@@ -0,0 +1,20 @@
+#******************************************************************************************************************************
+
+#content       = auto UV tool for Maya
+
+#version       = 0.1.1
+
+#date          = October 31, 2024
+
+#how to        = pending 
+
+#dependencies  = Maya.cmds
+
+#to do         = create the script 
+
+#author        = Elizabeth Guan <yijie.beth.guan@gmail.com>
+
+#******************************************************************************************************************************
+
+import pymel.core as pm 
+import maya.cmds as cmds 
